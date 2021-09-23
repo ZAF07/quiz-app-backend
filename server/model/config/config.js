8c045975945e7f8821b188dbe81945ed4f399653
ZAF07 <zaffere07@gmail.com>
2021-09-23 17:24:19 +0800

    heroku fix

--- server/model/config/config.js
+++ server/model/config/config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
     production: {
     dialect: 'postgres',
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: process.env.DATABASE_URL,
     dialectOptions: {
       ssl: {
         require: true,
