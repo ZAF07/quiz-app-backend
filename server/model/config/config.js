ba18563880f383d149af384d71fca1eccd20d099
ZAF07 <zaffere07@gmail.com>
2021-09-23 17:14:03 +0800

    added config for sequelize postgres

--- server/model/config/config.js
+++ server/model/config/config.js
@@ -12,7 +12,14 @@ module.exports = {
     database: 'quiz_app',
     host: '127.0.0.1',
     dialect: 'postgres',
-    use_env_variable: process.env.DATABASE_URL
+    use_env_variable: process.env.DATABASE_URL,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    }
   },
 
 };
+// postgres://unvgangnzrxdor:804abe4ee190833e028d99ee78fcf1aa01ad0d17eab48f1339e852950aa845dc@ec2-54-145-110-118.compute-1.amazonaws.com:5432/d6hgjattof63aj
