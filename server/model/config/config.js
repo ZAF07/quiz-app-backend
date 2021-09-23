08ab6db06b4524d3884a215f3ce498ac4b7902da
ZAF07 <zaffere07@gmail.com>
2021-09-23 17:20:15 +0800

    added config for sequelize postgres

--- server/model/config/config.js
+++ server/model/config/config.js
@@ -7,10 +7,6 @@ module.exports = {
     dialect: 'postgres',
   },
     production: {
-    username: 'zaffere',
-    password: null,
-    database: 'quiz_app',
-    host: '127.0.0.1',
     dialect: 'postgres',
     use_env_variable: 'DATABASE_URL',
     dialectOptions: {
